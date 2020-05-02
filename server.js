557ca45a2926bcde4a69e931fdc917c43da013ce
Ojhal Patwa <patkarojhal26@gmail.com>
2020-05-02 13:15:06 +0530

    Add route and funtionallty

--- server.js
+++ server.js
@@ -7,27 +7,48 @@ const cors = require('cors');
 const bodyParser = require('bodyParser');
 // Start up an instance of app
 const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
 app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
-const port = 8000;
-const server = app.listen(port, function() {
+const port = 8080;
+const server = app.listen(port, listening); 
+
+function listening(){
+    console.log('server running');
     console.log('running on localhost: ${port}');
-});
+};
+
 //GET route returns ProjectData
-app.get('/all', function (request, response) {
+app.get('/all', sendData) 
+
+function sendData (request, response) {
     response.send(projectData);
-});
+}
+
 // POST route adds data to projectData
-app.post('/add', function (request,response) {
+app.post('/addWeatherData', addData) 
     
+function addData(request, response) {
+    projectData.temperature = request.body.temperature;
+    projectData.date = request.body.date;
+    projectData.user_response = request.body.user_response;
+    response.end();
+    console.log(projectData)
+
 }
--- website/app.js
+++ website/app.js
@@ -1,7 +1,78 @@
 /* Global Variables */
+// Personal API key for OpenWeatherMap API
+let appID = '5e499f5ad8373ca656acff84e1ac77fe';
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-console.log(d);
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
+// Event listener  to add function 
+document.getElementById('generate').addEventListener('click', performAction);
+
+// Function called by event listener //
+function performAction(event) {
+    const zipCode = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    console.log(newDate);
+
+    getTemperature(baseURL, zipCode, appID)
+        .then(function (data) {
+            // Add data to POST request //
+            postData('http://localhost:8080/addWeatherData', { temperature: data.main.temp, date: newDate, user_response: feelings })
+                //Function which update UI
+                .then(function () {
+                    updateUI()
+
+                })
+        })
+}
+//Async GET
+const getTemperature = async (baseURL, code, appID) => {
+    const response = await fetch(baseURL + code + ',us' + '&APPID=' + appID)
+    console.log(response);
+    try {
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+    catch (error) {
+        console.log('error', error);
+    }
+}
+
+//Async POST
+const postData = async (url = '', data = {}) => {
+    const postRequest = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+
+    });
+    try {
+        
+        const newData = await postRequest.json();
+        console.log(newData);
+        return newData;
+    }
+    catch (error) {
+        console.log('Error', error);
+    }
+
+}
+// Update user interface //
+const updateUI = async () => {
+    const request = await fetch('http://localhost:8080/all');
+    try {
+        const allData = await request.json();
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = allData.temperature;
+        document.getElementById('content').innerHTML = allData.user_response;
+    }
+    catch (error) {
+        console.log('error', error);
+    }
+}
